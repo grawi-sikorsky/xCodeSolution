66322290a537b9c7a8f8adce53cb03b5d1e1c11e
HomePC <grawires@gmail.com>
2022-12-15 00:18:48 +0100

    imports cleanup

--- xcodesolution/src/main/java/pl/jsikora/xcodesolution/services/ExchangeRateService.java
+++ xcodesolution/src/main/java/pl/jsikora/xcodesolution/services/ExchangeRateService.java
@@ -1,15 +1,15 @@
 package pl.jsikora.xcodesolution.services;
 
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
 import pl.jsikora.xcodesolution.dto.RequestCurrencyDTO;
 import pl.jsikora.xcodesolution.dto.ResponseExchangeRateDTO;
 import pl.jsikora.xcodesolution.exceptions.BadExchangeResponseException;
 import pl.jsikora.xcodesolution.models.NBPCurrencyModel;
-
-import javax.swing.text.html.Option;
-import java.util.Map;
-import java.util.Optional;
 
 @Service
 public class ExchangeRateService {
